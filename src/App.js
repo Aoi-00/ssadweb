62d89420280690b066a12d17d6ce7211c9381b93
hyunatic <habbo_armageddon@hotmail.com>
2021-03-17 01:01:40 +0800

    prod

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBFooter, MDBNavLink } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ReactComponent as Logo } from './assets/logo.svg';
 import Routes from './router/Routes';
 import Store from './Redux/Store/Store';
 import { Provider } from 'react-redux';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 class App extends Component {
   state = {
     collapseID: ''
   };
-
   render() {
-  
-    const { collapseID } = this.state;
 
+    const { collapseID } = this.state;
     return (
       <Provider store={Store}>
         <Router>
